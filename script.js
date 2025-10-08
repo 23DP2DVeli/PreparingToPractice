b59f85df0269642c93b88180359517bac7be07f8
Daniels <A230255DV@rvt.lv>
2025-10-08 16:03:58 +0300

    Closures

---
+++ script.js
@@ -0,0 +1,30 @@
+function wallet (startBalance) {
+    balance = startBalance;
+
+
+    return {
+
+        add(amount) {
+            balance += amount
+            console.log(balance)
+        },
+
+        decrease(amount) {
+            balance -= amount
+            console.log(balance)
+        },
+
+        getBalance() {
+            console.log(balance)
+        }
+
+
+    }
+
+}
+
+const MyWallet = wallet(100)
+MyWallet.add(20)
+MyWallet.decrease(100)
+MyWallet.getBalance()
+console.log(MyWallet.balance)
