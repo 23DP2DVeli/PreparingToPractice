feea6e86ceb4241c435b569541297885ef7cd683
Daniels <A230255DV@rvt.lv>
2025-10-13 21:10:45 +0300

    Promise

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-console.log("start");
-
-setTimeout(() => console.log("task"), 0); // macrotask
-Promise.resolve().then(() => console.log("micro")); // microtask
-
-console.log("end");
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Done!"); // complete
+  }, 1000);
+});
