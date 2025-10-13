0a949abbee4f0ab257aed58aa44a3d279d951e90
Daniels <A230255DV@rvt.lv>
2025-10-13 21:13:14 +0300

    Resolve, Reject, End

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const myPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Done!"); // complete
+    resolve("Done!"); // выполнение прошло успешно
   }, 1000);
 });
+
+
+myPromise
+  .then(result => console.log("Result:", result))  // resolve
+  .catch(error => console.log("Err:", error))      // reject
+  .finally(() => console.log("End"))               // end
+
+
