be0bc8287431a1c83f8970e67f951f36da8f61b0
23DP2DVeli <daniels.veliks@icloud.com>
2026-02-16 14:08:56 +0200

    how to copy an array

--- repeatPlan.js
+++ repeatPlan.js
@@ -1,9 +1,7 @@
-const user = {
-  name: 'Jeffrey',
-  age: 30,
-}
+const arr = [1, 2, 3];
 
-const {name, age} = user
+const arr1 = JSON.parse(JSON.stringify(arr));
+arr1.push(4);
 
-
-console.log(name, age)
+console.log(arr1); 
+console.log(arr);  
