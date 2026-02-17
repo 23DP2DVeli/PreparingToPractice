00ffbf1c1ad58ba09447714824e2f52fe2b06466
23DP2DVeli <daniels.veliks@icloud.com>
2026-02-17 22:36:07 +0200

    1st task with objects

--- repeatPlan.js
+++ repeatPlan.js
@@ -1,6 +1,44 @@
-const arr = [1, 2, 3];
+// Создай объект user с полями:
+// name: 'Danik'
+// age: 18
+// city: 'Riga'
+// Создай массив чисел [1, 2, 3, 4, 5].
+// Напиши функцию updateUser, которая:
+// принимает объект пользователя
+// увеличивает возраст на 1
+// возвращает новый объект (не меняя оригинал) с помощью spread
 
-const arr1 = [...arr, 4]
+// Создай функцию processNumbers, которая:
+// принимает массив чисел
+// возвращает новый массив, где каждое число умножено на 2
+// использует map и не изменяет исходный массив
+// Выведи в консоль:
+// оригинальный user
+// новый объект пользователя
+// оригинальный массив чисел
+// новый массив чисел
 
-console.log(arr1); 
-console.log(arr);  
+
+const user = {
+  name: 'Danik',
+  age: 19,
+  city: 'Riga'
+}
+
+const arr = [1, 2, 3, 4, 5]
+
+function updateUser(person) {
+
+  const draftPerson = {...person}
+
+  draftPerson.age += 1
+
+  return draftPerson
+}
+
+const newUser = updateUser(user)
+
+
+console.log(user)
+console.log(newUser)
+
